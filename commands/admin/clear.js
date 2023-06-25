9aa8f702a785573f46dd2ffd27d90ccc7ed4d68e
Eliott-B <eliottb.info@gmail.com>
2023-06-25 10:08:10 +0200

    🪄 | Fix language

--- commands/admin/clear.js
+++ commands/admin/clear.js
@@ -5,22 +5,22 @@ const { informations, channels } = require('../../config.json');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('clear')
-        .setDescription('Supprime `i` messages du salon.')
+        .setDescription('Clear `i` messages.')
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
         .addIntegerOption(option =>
             option.setName('limite')
-            .setDescription('Donnez le nombre de messages à supprimer (100max).')
+            .setDescription('Enter the number of messages to delete (100max).')
             .setRequired(true)),
     async execute(interaction) {
         var i = interaction.options.getInteger('limite');
         if (i > 100){
-            await interaction.reply(`${interaction.member}, tu peux supprimer que 100 messages maximum !`);
+            await interaction.reply(`${interaction.member}, you can delete a maximum of 100 messages !`);
             return;
         }
         var logs = interaction.client.channels.cache.get(channels.logs);
         await interaction.channel.messages.fetch({limit: i}).then((messages) => {interaction.channel.bulkDelete(messages)});
-        await interaction.reply(`>>> *${i} messages ont bien été supprimés !*`);
-        var clear = embed('Clear','#f56816',`${interaction.member} a supprimé ${i} messages dans le salon : ${interaction.channel}`,null,informations.name,informations.logo);
+        await interaction.reply(`>>> *${i} messages have been successfully deleted !*`);
+        var clear = embed('Clear','#f56816',`${interaction.member} has deleted ${i} messages from the channel : ${interaction.channel}`,null,informations.name,informations.logo);
         logs.send({embeds: [clear]});
         setTimeout(async function(){
             await interaction.deleteReply();
